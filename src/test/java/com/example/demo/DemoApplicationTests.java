01e13061b8c19fa96250d58f298b6a1c3fd1fc44
haakon <haakon_underdal@hotmail.com>
2019-10-07 13:20:54 +0200

    added yml file for testing and added a simple test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assertions.assertEquals(true,true);
 	}
 
 }
