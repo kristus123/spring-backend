5cc92e2ee428b579729af47edf9f96d1d698e8d7
krispetter@gmail.com <krispetter@gmail.com>
2019-10-09 10:18:00 +0200

    changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	// ${ ENVIRONMENT : dev }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
