03f5f5e7fb8db6a2886b79525a9bbd785ad36648
Inthu <inthu.raveendran@ciber.no>
2019-10-14 03:45:55 +0200

    workaround for access denied

--- src/main/java/com/example/demo/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configurations/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().and().cors().and().csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/api/public/**").permitAll()
-                    .antMatchers("/v1/users/signup").permitAll()
+                    .antMatchers("/v1/**").permitAll()
                     .antMatchers("/api/authenticate").permitAll()
                     .antMatchers("/hello").permitAll()
 
