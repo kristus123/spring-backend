a05afa5bb863160972c7c5d09553fda168fd423b
krispetter@gmail.com <krispetter@gmail.com>
2019-10-18 10:32:09 +0200

    changes

--- src/test/java/com/example/demo/controllers/adminControllers/AdministratorAssociationControllerTest.java
+++ src/test/java/com/example/demo/controllers/adminControllers/AdministratorAssociationControllerTest.java
@@ -5,6 +5,7 @@ import com.example.demo.repositories.AssociationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
