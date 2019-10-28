540b41b34185790e264342126ea9660b32481637
haakon <haakon_underdal@hotmail.com>
2019-10-28 18:04:55 +0100

    added some endpoints

--- src/main/java/com/example/demo/controllers/commonControllers/CommonMatchController.java
+++ src/main/java/com/example/demo/controllers/commonControllers/CommonMatchController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers.commonControllers;
 
 import com.example.demo.assembler.MatchResourceAssembler;
+import com.example.demo.dtos.MatchDTO;
+import com.example.demo.dtos.MatchGoalDTO;
+import com.example.demo.dtos.MatchResultDTO;
 import com.example.demo.exceptions.ElementNotFoundException;
 import com.example.demo.models.MatchModel;
 import com.example.demo.services.MatchService;
@@ -11,6 +14,8 @@ import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -39,6 +44,17 @@ public class CommonMatchController {
 
         return ResponseEntity
                 .ok(resource);
+    }
+
+    @GetMapping("/get/match/{teamId}/byTeam")
+    public List<HashMap<String, Object>> getMatchesByTeamId (@PathVariable int teamId) {
+
+        List<MatchModel> matches = matchService.findByTeam(teamId);
+        List<HashMap<String, Object>> l = new ArrayList<>();
+        matches.forEach(match -> {
+            l.add( matchService.getFilteredMatchStats(match, teamId));
+        });
+        return l;
     }
 
     @GetMapping("/get/match")
