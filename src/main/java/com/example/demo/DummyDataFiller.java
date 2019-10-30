9c0f51258d56a45c4c190164345e020fef0d6bd7
haakon <haakon_underdal@hotmail.com>
2019-10-30 17:49:36 +0100

    added data

--- src/main/java/com/example/demo/DummyDataFiller.java
+++ src/main/java/com/example/demo/DummyDataFiller.java
@@ -45,6 +45,10 @@ public class DummyDataFiller implements CommandLineRunner {
         // -------/----------- CREATING TEAMS ------------------ //
         TeamModel team_1 = dummyDataService.createTeam("Barcelona", "Camp Nou", "persons_barcelona");
         TeamModel team_2 =  dummyDataService.createTeam("Real Madrid", "Estadio Santiago Bernabéu", "persons_madrid");
+        TeamModel team_3 =  dummyDataService.createTeam("Juventus", "Juventus Stadium", "persons_madrid");
+        TeamModel team_4 =  dummyDataService.createTeam("Napoli", "San Paolo", "persons_madrid");
+        TeamModel team_5 =  dummyDataService.createTeam("Lyon", "Parc Olympique lyonnais", "persons_madrid");
+        TeamModel team_6 =  dummyDataService.createTeam("Olympique de Marseille", "Stade Vélodrome", "persons_madrid");
 
         // ------------------ CREATING MATCH ------------------ //
         SeasonModel season = new SeasonModel(LocalDate.of(2015, 01, 01),
@@ -57,8 +61,24 @@ public class DummyDataFiller implements CommandLineRunner {
                 "La Liga 2",
                 "Top division in the spanish league. Renowned for its acting");
 
+        SeasonModel season_3 = new SeasonModel(LocalDate.of(2015, 01, 01),
+                LocalDate.of(2020, 01,01),
+                "Lig a",
+                "Top division in the France");
+
+        SeasonModel season_4 = new SeasonModel(LocalDate.of(2015, 01, 01),
+                LocalDate.of(2020, 01,01),
+                "Seria A",
+                "Top division in the Italy");
+
         dummyDataService.createMatch(season, team_1, team_2, "matchGoals_1");
         dummyDataService.createMatch(season_2, team_1, team_2, "matchGoals_1");
+
+        dummyDataService.createMatch(season_3, team_3, team_4, "matchGoals_1");
+        dummyDataService.createMatch(season_3, team_4, team_3, "matchGoals_1");
+
+        dummyDataService.createMatch(season_4, team_5, team_6, "matchGoals_1");
+        dummyDataService.createMatch(season_4, team_6, team_5, "matchGoals_1");
 
     }
 }
